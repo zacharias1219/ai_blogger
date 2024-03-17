f8576a9a0381b17b463f381baed5c37b32a5c2c9
Richard Abishai <rickoshade1891@gmail.com>
2024-03-17 22:40:26 +0530

    code upgrade

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ model = genai.GenerativeModel(model_name="gemini-1.0-pro",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
-st.set_page_config(layout = "wide")
+st.set_page_config(page_title="AI Blogger",layout = "wide")
 st.title("Blogcraft")
 st.subheader("Craft perfect blog using AI")
 
@@ -52,10 +52,12 @@ with st.sidebar:
 
     language = st.selectbox("Language", ["English", "Hindi", "Spanish", "French", "German", "Italian", "Portuguese", "Dutch", "Russian", "Japanese", "Chinese"])
 
-    num_words = st.number_input("Number of words", min_value=200, max_value=1000, step=100)
+    num_words = st.number_input("Number of words", min_value=200, max_value=2000, step=100)
+
+    extra_datails = st.text_area("Extra details")
 
     prompt_parts = [
-        f"Craft an all-encompassing and captivating blog post tailored to the provided {blog_title} and {keywords}. The post should span approximately {num_words} words, catering to an online readership. Emphasize originality, provide valuable insights, and sustain a coherent tone across the piece. The blog post should be written in {language} and should be optimized for search engines.  The post should be written in a {text_style} style, and should be free from any grammatical errors or plagiarism."
+        f"Craft an all-encompassing and captivating blog post tailored to the provided {blog_title} and {keywords}. The post should span approximately {num_words} words, catering to an online readership. Emphasize originality, provide valuable insights, and sustain a coherent tone across the piece. The blog post should be written in {language} and should be optimized for search engines.  The post should be written in a {text_style} style, and should be free from any grammatical errors or plagiarism. if there is any extra details, it should be incorporated into the blog post. {extra_datails}"
     ]
 
     
